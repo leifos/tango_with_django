b0d9ca27702ef0eb448a060da69b7a83a0ecd300
Stuart Hertzog <stuart@stuzog.com>
2017-11-11 21:10:46 -0800

    Update populate_rango.py
    Change to run with Python 3.6.2 -- existing one won't work without these changes.

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -42,19 +42,21 @@ def populate():
     # Print out what we have added to the user.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print "- {0} - {1}".format(str(c), str(p))
+            print("- {0} - {1}".format(str(c), str(p)))
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
 def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 # Start execution here!
 if __name__ == '__main__':
-    print "Starting Rango population script..."
+    print("Starting Rango population script...")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+    import django
+    django.setup()
     from rango.models import Category, Page
     populate()
